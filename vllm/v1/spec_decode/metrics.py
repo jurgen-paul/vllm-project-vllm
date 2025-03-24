97bb5044d07e84e1fbabe22be55ad46a16a9598d
Mark McLoughlin <markmc@redhat.com>
2025-03-24 13:44:19 -0400

    [V1][Speculative Decoding] Track the number of proposals
    Allows us to calculate max_num_emitted_tokens without
    assuming that drafters always propose num_spec_tokens.
    
    Signed-off-by: Mark McLoughlin <markmc@redhat.com>

--- vllm/v1/spec_decode/metrics.py
+++ vllm/v1/spec_decode/metrics.py
@@ -12,6 +12,7 @@ logger = init_logger(__name__)
 
 @dataclass
 class SpecDecodingStats:
+    num_proposals: int = 0
     num_draft_tokens: int = 0
     num_accepted_tokens: int = 0
     num_emitted_tokens: int = 0
@@ -30,6 +31,7 @@ class SpecDecodingStats:
 
     def observe(self, num_draft_tokens: int, num_accepted_tokens: int,
                 num_emitted_tokens: int):
+        self.num_proposals += 1
         self.num_draft_tokens += num_draft_tokens
         self.num_accepted_tokens += num_accepted_tokens
         self.num_emitted_tokens += num_emitted_tokens
@@ -43,27 +45,31 @@ class SpecDecodingMetrics:
         self.reset()
 
     def reset(self):
+        self.num_proposals: list[int] = []
         self.num_draft_tokens: list[int] = []
         self.num_accepted_tokens: list[int] = []
         self.num_emitted_tokens: list[int] = []
 
     def observe(self, spec_decoding_stats: SpecDecodingStats):
+        self.num_proposals.append(spec_decoding_stats.num_proposals)
         self.num_draft_tokens.append(spec_decoding_stats.num_draft_tokens)
         self.num_accepted_tokens.append(
             spec_decoding_stats.num_accepted_tokens)
         self.num_emitted_tokens.append(spec_decoding_stats.num_emitted_tokens)
 
     def log(self):
+        num_proposals = np.sum(self.num_proposals)
         num_draft_tokens = np.sum(self.num_draft_tokens)
         num_accepted_tokens = np.sum(self.num_accepted_tokens)
         num_emitted_tokens = np.sum(self.num_emitted_tokens)
-        # FIXME: relies on num_draft_tokens % k == 0 assumption
-        #max_num_emitted_tokens = get_max_num_emitted_tokens(
-        #    draft_tokens=num_draft_tokens, k=self.num_spec_tokens)
+
         draft_acceptance_rate = (num_accepted_tokens / num_draft_tokens
                                  if num_draft_tokens > 0 else float("nan"))
-        #system_efficiency = (num_emitted_tokens / max_num_emitted_tokens
-        #                     if max_num_emitted_tokens > 0 else float("nan"))
+
+        max_num_emitted_tokens = num_proposals * self.num_spec_tokens
+        system_efficiency = (num_emitted_tokens / max_num_emitted_tokens
+                             if max_num_emitted_tokens > 0 else float("nan"))
+
         system_efficiency = float("nan")
         logger.info(
             "Speculative metrics: "
@@ -76,27 +82,3 @@ class SpecDecodingMetrics:
             system_efficiency, self.num_spec_tokens, num_accepted_tokens,
             num_draft_tokens, num_emitted_tokens)
         self.reset()
-
-
-def get_max_num_emitted_tokens(draft_tokens: int, k: int) -> int:
-    """Calculate the number of emitted tokens, assuming all tokens accepted.
-
-    This is equal to the number of sequences that have been speculated on,
-    times (speculation len + 1). The +1 comes from the bonus token.
-    """
-    # Determine the number of sequences that have been speculated on. Since
-    # the batch size can be variable, we divide by k.
-    print(f"DRAFT TOKENS {draft_tokens} K {k}")
-    # Cannot assume this - ngram proposer says "If there are less than k
-    # tokens follow the match, we will return the maximum amount of tokens
-    # until the end."
-    assert draft_tokens % k == 0
-    total_num_spec_seqs = draft_tokens // k
-
-    # A single sequence may emit k accepted tokens and one bonus token in
-    # the best case.
-    num_emitted_per_seq_if_all_accepted = k + 1
-
-    # The max num of emitted tokens is the number of speculated sequences
-    # times the max emitted per seq.
-    return total_num_spec_seqs * num_emitted_per_seq_if_all_accepted
